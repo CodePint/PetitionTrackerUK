4f99f2295d2b04602de4c9de5bb6fe687f9f12ae
Code Pint <geddie.code@gmail.com>
2020-08-07 15:06:33 +0100

    - added chart.js to react frontend - created a chart component folder - added a basic bar chart dummy config - added chart component with ability to dynamically update - integrated the chart component into petition component and lifted state up - created dummy fetchData function which updates the chart component onClick
    -- Next steps:
      - create a lineChartConfig.jsx
      - input total signatures over time into the line chart
      - implement dynamic load and updates for the barchart

--- frontend/src/components/Petition.jsx
+++ frontend/src/components/Petition.jsx
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import JSONPretty from "react-json-pretty";
+import axios from "axios";
 import "./css/Petition.css";
+
+import Chart from "./charts/Chart";
+// import { lineChartConfig } from "./charts/LineChartConfig";
 
 function Petition({ match }) {
   const petition_id = match.params.petition_id;
   const [petition, setPetition] = useState({});
   const [latestRecord, setlatestRecord] = useState({});
   const [latestRecords, setLatestRecords] = useState({});
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     fetchPetition();
+    fetchData();
   }, []);
+
+  // dummy function for fetching data from the API
+  const fetchData = () => {
+    const randomInt = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    const data = [
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+    ];
+    setChartData(data);
+  };
 
   async function fetchPetition() {
     try {
@@ -29,19 +48,33 @@ function Petition({ match }) {
   return (
     <div className="Petition">
       <h1>Petition ID: {petition_id}</h1>
-      <h2>Petition data:</h2>
-      <div>
-        <JSONPretty id="json-pretty" data={petition}></JSONPretty>
+      <h1>Action: {petition["action"]}</h1>
+
+      <div className="PetitionChart">
+        <h2>Total signatures</h2>
+        <div className="UpdateChart">
+          <button onClick={fetchData}>Fetch Random Data!</button>
+        </div>
+        <div className="ChartWrapper">
+          <Chart chartData={chartData} />
+        </div>
       </div>
 
-      <h2>Latest Records ({latestRecords.length}):</h2>
-      <div>
-        <JSONPretty id="json-pretty" data={latestRecords}></JSONPretty>
-      </div>
+      <div className="data">
+        <h2>Petition data:</h2>
+        <div>
+          <JSONPretty id="json-pretty" data={petition}></JSONPretty>
+        </div>
 
-      <h2>Lastest Record:</h2>
-      <div>
-        <JSONPretty id="json-pretty" data={latestRecord}></JSONPretty>
+        <h2>Latest Records ({latestRecords.length}):</h2>
+        <div>
+          <JSONPretty id="json-pretty" data={latestRecords}></JSONPretty>
+        </div>
+
+        <h2>Lastest Record:</h2>
+        <div>
+          <JSONPretty id="json-pretty" data={latestRecord}></JSONPretty>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/charts/Chart.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from "react";
+import Chartjs from "chart.js";
+import "./css/Chart.css";
+import { barChartConfig } from "./BarChartConfig";
+
+function Chart({ chartData }) {
+  const chartContainer = useRef(null);
+  const chartConfig = barChartConfig;
+  const [chartInstance, setChartInstance] = useState(null);
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
+
+  useEffect(() => {
+    if (chartInstance) {
+      chartInstance.data.datasets[0].data = chartData;
+      chartInstance.update();
+    }
+  });
+
+  return (
+    <div className="Chart">
+      <canvas ref={chartContainer} />
+    </div>
+  );
+}
+
+export default Chart;
