bd7cee5d203645b1a8788715b6b3eb7402a3713d
Code Pint <geddie.code@gmail.com>
2020-11-09 18:48:15 +0000

    updating tracker tasks to use new TaskRun execution context

--- application/tracker/tasks.py
+++ application/tracker/tasks.py
@@ -1,9 +1,8 @@
-from .models import Petition, Record
-from .models import PetitionSchema, RecordSchema
 from flask import current_app as c_app
 from application import celery
-from celery_once import QueueOnce
-
+from .models import Petition, Record
+from .models import PetitionSchema, RecordSchema
+from application.models import TaskRun as Run
 import datetime as dt
 import os, logging
 
@@ -16,42 +15,42 @@ once_opts = {
 }
 
 @celery.task(name='onboard_task', **celery_opts, **once_opts)
-def onboard_self_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+def onboard_self_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         petition = Petition.onboard(id)
-        return runner.commit(result=petition, schema=PetitionSchema())
+        return handler.commit(result=petition, schema=PetitionSchema())
 
 @celery.task(name='poll_self_task', **celery_opts, **once_opts)
-def poll_self_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+def poll_self_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         petition = Petition.query.get(kwargs["id"])
         record = petition.poll_self(**kwargs)
-        return runner.commit(result=record, schema=RecordSchema())
+        return handler.commit(result=record, schema=RecordSchema())
 
 @celery.task(name='base_poll_task', **celery_opts, **once_opts)
-def base_poll_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+def base_poll_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         records = Petition.poll(**kwargs)
         logger.info("Petitions polled: {}".format(len(records)))
-        return runner.commit([r.petition_id for r in records])
+        return handler.commit(result=[r.petition_id for r in records])
 
 @celery.task(name='geo_poll_task', **celery_opts, **once_opts)
-def geo_poll_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+def geo_poll_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         records = Petition.poll(geo=True **kwargs)
         logger.info("Petitions polled: {}".format(len(records)))
-        return runner.commit([r.petition_id for r in records])
+        return handler.commit(result=[r.petition_id for r in records])
 
-@celery.task(name='populate_petitions_task', **celery_opts, **once_opts)
-def populate_petitions_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+@celery.task(name='populate_task', **celery_opts, **once_opts)
+def populate_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         petitions = Petition.populate(**kwargs)
         logger.info("Petitions onboarded: {}".format(len(petitions)))
-        return runner.commit([p.id for p in petitions])
+        return handler.commit(result=[p.id for p in petitions])
 
 @celery.task(name='update_trending_pos_task', **celery_opts, **once_opts)
-def update_trending_pos_task(self, runner=None, *args, **kwargs):
-    with runner.execute(bind=self):
+def trending_pos_task(self, *args, **kwargs):
+    with Run.execute(bind=self) as handler:
         petitions = Petition.update_trending(logger=self.logger, **kwargs)
         logger.info("Petitions updated: {}".format(len(petitions)))
-        return runner.commit([p.id for p in petitions])
+        return handler.commit(result=[p.id for p in petitions])
