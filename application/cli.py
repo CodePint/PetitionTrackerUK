1b0692ed98c4868c43ccf13bca99e7077a1686e6
Code Pint <geddie.code@gmail.com>
2020-11-09 18:42:44 +0000

    - rewrote flask cli to be more user friendly and make use of click - wrote an untested compose cli wrapper for use with new service   specific compose files and to aid deployment/container management

--- application/cli.py
+++ application/cli.py
@@ -3,80 +3,136 @@ import subprocess, click, os
 from application import context
 
 def register(app, db, celery):
-    cli = CliFunction(app, db, celery)
+    cli_func = CliFunction(app, db, celery)
 
-    @app.cli.command("init-settings")
-    def cli_init_settings():
-        print("configuring default values for settings table")
-        app.settings.configure(app.config["DEFAULT_SETTINGS"])
+    task_cli(app, db, celery, cli_func)
+    db_cli(app, db, celery, cli_func)
+    exec_cli(app, db, celery, cli_func)
 
-    @app.cli.command("init-task-schedules")
-    def cli_init_task_schedule():
-        print("initializing task schedules")
-        app.celery_utils.init_schedule(overwrite=True)
+    @app.cli.command("--init-settings")
+    def init_settings():
+        click.echo("configuring default values for settings table")
+        app.models.Setting.configure(app.config["DEFAULT_SETTINGS"])
 
-    @app.cli.command("init-task-templates")
-    def cli_init_task_schedule():
-        print("initializing task templates")
-        app.celery_utils.init_templates(overwrite=True)
-
-    @app.cli.command("run-tracker-tasks")
-    def cli_run_overdue_tasks():
-        print("checking for overdue tracker tasks")
-        app.celery_utils.run_tasks_for(queue="tracker")
-
-    @app.cli.command("react")
-    def cli_run_yarn():
-        print("starting react frontend")
-        subprocess.run("cd frontend && yarn run start", shell=True)
-
-    @app.cli.command("update-geographies")
-    def cli_update_geography_data():
-        print("updating geography application choices")
+    @app.cli.command("--update-geographies")
+    def update_geography_data():
+        click.echo("updating geography application choices")
         from application.tracker import geographies
 
-    @app.cli.command("db-check")
-    def cli_check_db():
-        print("checking tables")
-        cli.check_tables()
 
-    @app.cli.command("db-create")
-    def cli_create_db():
-        print("creating database!")
+
+def exec_cli(app, db, celery, cli_func):
+
+    SCRIPTS_DIR = app.config["PROJ_SCRIPTS_DIR"]
+
+    @app.cli.group("exec")
+    def exec_group():
+        pass
+
+    @exec_group.command()
+    def react():
+        click.echo("starting react frontend")
+        subprocess.run("cd frontend && yarn run start", shell=True)
+
+    @exec_group.command()
+    def gunicorn():
+        gunicorn = os.path.join(SCRIPTS_DIR, "flask", "wsgi")
+        subprocess.run(f"{gunicorn}.sh")
+
+    @exec_group.group("celery")
+    def celery_group():
+        pass
+
+    @click.option("--name", required=True)
+    @celery_group.command("worker")
+    def worker(name):
+        worker = os.path.join(SCRIPTS_DIR, "celery/workers", name)
+        subprocess.run(f"{worker}.sh")
+
+    @celery_group.command()
+    def beat():
+        beat = os.path.join(SCRIPTS_DIR, "celery", "beat")
+        subprocess.run(f"{beat}.sh")
+
+    @celery_group.command()
+    def flower():
+        flower = os.path.join(SCRIPTS_DIR, "celery", "flower")
+        subprocess.run(f"{flower}.sh")
+
+
+
+def db_cli(app, db, celery, cli_func):
+
+    @app.cli.group("db-cli")
+    def db_cli_group():
+        pass
+
+    @db_cli_group.command()
+    def check():
+        click.echo("checking tables")
+        cli_func.check_tables()
+
+    @db_cli_group.command()
+    def create():
+        click.echo("creating database!")
         db.create_all()
 
-    @app.cli.command("db-drop")
-    def cli_drop_db():
-        cli.drop_tables()
-        print("database drop completed")
+    @db_cli_group.command("drop-all")
+    def drop_all():
+        cli_func.drop_tables()
 
-    @app.cli.command("db-drop-alembic")
-    def cli_reset_alembic():
-        cli.drop_alembic()
+    @click.argument("--models", nargs=-1)
+    @db_cli_group.command()
+    def drop(models):
+        cli_func.drop_tables(models)
 
-    @app.cli.command("delete-records")
-    def cli_delete_records():
-        cli.delete_all_from("Record")
+    @db_cli_group.command("reset-alembic")
+    def reset_alembic():
+        cli_func.drop_alembic()
 
-    @app.cli.command("delete-petitions")
-    def cli_delete_records():
-        cli.delete_all_from("Petition")
+    @click.option("--model")
+    @db_cli_group.command()
+    def delete(model):
+        cli_func.delete_all_from(model)
 
-    @app.cli.command("delete-signatures")
-    def cli_delete_records():
-        cli.delete_all_from("SignaturesByCountry")
-        cli.delete_all_from("SignaturesByRegion")
-        cli.delete_all_from("SignaturesByConstituency")
 
-    @app.cli.command("purge-celery")
-    def cli_purge_celery():
-        print("purging celery!")
-        app.celery.control.purge()
 
-    @app.cli.command("clear-task-runs")
-    def cli_clear_task_runs():
-        print("purging tasks!")
-        app.models.Task.purge_all()
+def task_cli(app, db, celery, cli_func):
+
+    @app.cli.group("tasks")
+    def tasks_group():
+        pass
+
+    @click.option("--schedule/--no-schedule", default=False)
+    @click.option("--templates/--no-templates", default=False)
+    @tasks_group.command("init")
+    def init(schedule, templates):
+        if schedule:
+            app.celery_utils.init_schedule(overwrite=True)
+        if templates:
+            app.celery_utils.init_templates(overwrite=True)
+
+    @click.option("--name", required=True)
+    @click.option("--key", required=True)
+    @tasks_group.command()
+    def send(name, key):
+        app.celery_utils.send_task(name=name, key=key)
+
+    @click.option("--all-global/--no-all-global", default=False)
+    @click.option("--name")
+    @click.option("--key")
+    @tasks_group.command()
+    def revoke(all_global, name, key):
+        if all_global:
+            click.echo("revoking all tasks!")
+            app.models.Task.revoke_all()
+        if name and key:
+            task = app.models.Task.get(name=name, key=key)
+            if task:
+                click.echo(f"revoking task: {name}/{key}")
+                task.revoke()
+            else:
+                click.echo(f"task not found: {name}/{key}")
 
 
 
@@ -88,11 +144,11 @@ class CliFunction():
         self.celery = celery
 
     def drop_alembic(self):
-        print("dropping alembic database table!")
+        click.echo("dropping alembic database table!")
         self.db.engine.connect().execute("DROP TABLE IF EXISTS alembic_version")
 
     def drop_tables(self, models=None):
-        print("attempting to dropping tables!")
+        ("attempting to dropping tables!")
 
         with self.app.app_context():
             models = models or context.import_models()
@@ -104,15 +160,15 @@ class CliFunction():
                         model.__table__.drop(self.db.engine)
                         dropped.append(name)
                     else:
-                        print("Table: '{}', does not exist".format(name))
+                        click.echo("Table: '{}', does not exist".format(name))
                 except Exception as error:
-                    print("Exception while dropping: '{}', error: {}".format(name, error))
+                    click.echo("Exception while dropping: '{}', error: {}".format(name, error))
                     failed[name] = model
 
-            print("Tables dropped: {}".format(dropped))
+            click.echo("Tables dropped: {}".format(dropped))
 
             if any(failed):
-                print("Retying drop_tables for: {}".format(failed.values()))
+                click.echo("Retying drop_tables for: {}".format(failed.values()))
                 self.drop_tables(failed)
             else:
                 return dropped
@@ -125,23 +181,23 @@ class CliFunction():
                 if self.app.db.engine.has_table(model.__tablename__)
             ]
 
-            print("Tables ({}/{}) found: {}".format(len(tables), len(app_models), tables))
+            click.echo("Tables ({}/{}) found: {}".format(len(tables), len(app_models), tables))
             return tables
 
     def delete_all_from(self, name):
         model = context.import_models()[name]
         og_count = model.query.count()
-        print("deleting all: {} ({})".format(name, og_count))
+        click.echo("deleting all: {} ({})".format(name, og_count))
 
         try:
             model.query.delete()
         except Exception as error:
             count = model.query.count()
-            print("error while deleting, remaining: {}. error: {}".format(count, error))
+            click.echo("error while deleting, remaining: {}. error: {}".format(count, error))
 
         self.app.db.session.commit()
         count = model.query.count()
         if count == 0:
-            print("delete successful! deleted: {}".format(og_count))
+            click.echo("delete successful! deleted: {}".format(og_count))
         else:
-            print("delete incomplete, remaining: {}".format(count))
+            click.echo("delete incomplete, remaining: {}".format(count))
