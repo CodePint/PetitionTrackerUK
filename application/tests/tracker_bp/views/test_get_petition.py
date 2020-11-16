b54a09455d5130c4e9f91494a7613995de975fcd
Code Pint <geddie.code@gmail.com>
2020-11-16 14:37:25 +0000

    - updating project git ignore with better glob patterns and removed old rules - added git ignore to tracker seed remotes - deleting commented lines in tracker tests

--- application/tests/tracker_bp/views/test_get_petition.py
+++ application/tests/tracker_bp/views/test_get_petition.py
@@ -10,18 +10,3 @@ class TestGetPetition():
 
     def with_signatures(self):
         pass
-
-# def test_get_petitions_list():
-#     pass
-
-# def test_get_petition_signatures():
-#     pass
-
-# def test_get_petition_signatures_by_geography():
-#     pass
-
-# def test_get_petition_signatures_by_locale():
-#     pass
-
-# def test_get_petition_signatures_comparison():
-#     pass
