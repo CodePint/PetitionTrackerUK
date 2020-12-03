a9cc7c01a130622717f1085a0ccd2cf9940b7476
Code Pint <geddie.code@gmail.com>
2020-12-03 12:45:01 +0000

    restructuring tracker bp test directories

---
+++ application/tests/tracker/tests/views/test_get_petition.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+from flask import current_app
+from pytest_mock import mocker
+
+class TestGetPetition():
+
+    def test_get_without_signatures(self):
+        pass
+
+    def test_get_with_signatures(self):
+        pass
